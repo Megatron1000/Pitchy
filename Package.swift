7ad1357c44cbc13f29cfed39d7ff6a1621b5de71
Mark Bridges <mark@bridgetech.io>
2020-09-28 16:30:45 +0100

    Package tools version format update

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-  name: "Pitchy"
+    name: "Pitchy",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "Pitchy-iOS", targets: ["Pitchy-iOS"]),
+        .library(name: "Pitchy-Mac", targets: ["Pitchy-Mac"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Pitchy-iOS", dependencies: []),
+        .testTarget(name: "Pitchy-iOS-Tests", dependencies: []),
+        .target(name: "Pitchy-Mac", dependencies: []),
+        .testTarget(name: "Pitchy-Mac-Tests", dependencies: []),
+    ]
 )
